dd60bceee72d5cb3857bdf6ed6c493905e22f4af
Davide Colombo <davide.colombo02@protonmail.com>
2023-07-31 12:57:00 +0200

    nlines.s: inspection of original version
    The version has been taken from "the C programming language" book
    from Ritchie & Kernighan

--- nlines.c
+++ nlines.c
@@ -5,12 +5,37 @@
 
 int main(){
 
-    int nlines = 0;
     int c;
-    do{
-        c = getchar();
-        if(c == NEWLINE) nlines = nlines + 1;
-    } while(c != EOF);
+    int nlines = 0;
 
-    printf("The total number of lines is %d\n", nlines);
+    // nlines.s
+    // using O3 optimization
+    
+    /*
+    WHILE:
+        branch and link -> getchar
+        compare with EOF
+        branch equal to CLEANUP
+
+     ;; second block (enters the while loop)
+        compare character with target
+        branch not equal to WHILE
+
+    ;; third block (enters the if)
+        increment counter
+        jump to WHILE
+
+    CLEANUP:
+        move result into x0
+        restore the stack
+        return 
+    */
+
+    while((c = getchar()) != EOF)
+        if(c == NEWLINE)
+            ++nlines;
+    
+    // Commented just for looking at the assembly generated
+    // printf("nlines = %d\n", nlines);
+    return nlines;
 }
