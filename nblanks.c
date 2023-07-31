67e1140c8f037308134f1de8ea1cc45df142ce20
Davide Colombo <davide.colombo02@protonmail.com>
2023-07-31 18:42:40 +0200

    nblanks.s: inspect the while version

--- nblanks.c
+++ nblanks.c
@@ -6,9 +6,43 @@ int main(){
     int c;
     long nchars;
 
+    /////////////////////////////////////////////////////////////////
+    // nblanks.s
+    // compiled with O2 level
+
+    /*
+    ;; setup block
+        ...
+    
+    INC:
+        increment
+    
+    GETCHAR:
+        branch and link -> getchar()
+        substract #9 to the character
+        compare the result of subtraction with #2 (unsigned 32-bit comparison)
+        branch if lower (b.lo) to INC
+    
+    ;; block 3
+        compare char to -1 (EOF)
+        branch equal to PRINTF
+
+    ;; block 4
+        unsigned 32-bit comparison of character with #32 (space in ASCII)
+        branch not equal to GETCHAR
+        jump to INC
+
+    ;; cleanup block
+        ...
+    */
+
+   // C code
     nchars = 0;
     while((c = getchar()) != EOF)
         if(c == '\t' || c == ' ' || c == '\n')
             ++nchars;
-    printf("Total blanks, tabs, lines is %ld\n", nchars);
+
+    // Commented out for inspecting assembly file
+    //printf("Total blanks, tabs, lines is %ld\n", nchars);
+    return nchars;
 }
