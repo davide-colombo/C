9c5bbb062ba5f3410e8baf3e6141de584e499440
Davide Colombo <davide.colombo02@protonmail.com>
2023-07-31 19:58:38 +0200

    nblanks.s: better version with do-while

--- nblanks.c
+++ nblanks.c
@@ -37,12 +37,25 @@ int main(){
     */
 
    // C code
-    nchars = 0;
+    /*nchars = 0;
     while((c = getchar()) != EOF)
         if(c == '\t' || c == ' ' || c == '\n')
-            ++nchars;
+            ++nchars;*/
 
     // Commented out for inspecting assembly file
     //printf("Total blanks, tabs, lines is %ld\n", nchars);
+
+    /////////////////////////////////////////////////////////////////
+    // nblanks.s, do-while version
+    // compiled with O2 level
+
+    // The generated instructions are much better, without bubbles due to branches.
+
+    // C code
+    do{
+        c = getchar();
+        if(c == '\t' || c == '\n' || c == ' ') nchars = nchars + 1;
+    } while(c != EOF);
+
     return nchars;
 }
