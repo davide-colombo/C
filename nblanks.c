a48ab41ebb6220f0a6948fb80493f25ebb9b7957
Davide Colombo <davide.colombo02@protonmail.com>
2023-08-03 14:57:24 +0200

    nblanks.s: back to the version with 3 independent counters

--- nblanks.c
+++ nblanks.c
@@ -8,73 +8,23 @@
 
 int main(){
 
-    /////////////////////////////////////////////////////////////////
-    // nblanks.s
-    // compiled with O2 level
-
-    /*
-    ;; setup block
-        ...
-    
-    INC:
-        increment
-    
-    GETCHAR:
-        branch and link -> getchar()
-        substract #9 to the character
-        compare the result of subtraction with #2 (unsigned 32-bit comparison)
-        branch if lower (b.lo) to INC
-    
-    ;; block 3
-        compare char to -1 (EOF)
-        branch equal to PRINTF
-
-    ;; block 4
-        unsigned 32-bit comparison of character with #32 (space in ASCII)
-        branch not equal to GETCHAR
-        jump to INC
-
-    ;; cleanup block
-        ...
-    */
-
-   // C code
-    /*nchars = 0;
-    while((c = getchar()) != EOF)
-        if(c == '\t' || c == ' ' || c == '\n')
-            ++nchars;*/
-
-    // Commented out for inspecting assembly file
-    //printf("Total blanks, tabs, lines is %ld\n", nchars);
-
-    /////////////////////////////////////////////////////////////////
-    // nblanks.s, do-while version
-    // compiled with O2 level
-
     int32_t c;
-    uint64_t nchars;
-    nchars = 0;
+    uint32_t cmp;
 
+    uint64_t nl, nt, ns;
+    nl = nt = ns = 0;
     do{
         c = getchar();
-        //nl += !((c ^ NEWL) | (c & ~NEWL));      // same as (c == NEWL)
-        //ns += !((c ^ SPCE) | (c & ~SPCE));      // same as (c == SPCE)
-        //nt += !((c ^ HTAB) | (c & ~HTAB));      // same as (c == HTAB)
-        uint32_t nwl_eq = (c == NEWL);
-        uint32_t spc_eq = (c == SPCE);
-        uint32_t tab_eq = (c == HTAB);
 
-        nchars += nwl_eq + spc_eq + tab_eq;
-
-        /*nl += (c == NEWL);
+        nl += (c == NEWL);
         ns += (c == SPCE);
-        nt += (c == HTAB);*/
+        nt += (c == HTAB);
+
     } while(c != EOF);
     
     // Make the two ADD instructions independent
-    //uint64_t nchars_1 = nl + ns;
-    //uint64_t nchars_2 = nt + 0;
-    //uint64_t nchars = nchars_1 + nchars_2;
+    uint64_t nchars_1 = nl + ns;
+    uint64_t nchars = nchars_1 + nt;
 
     printf("nchars = %llu\n", nchars);
     return 0;
