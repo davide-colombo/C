4f40e5b35f7ff5b3221809a8eb26ecf99bce4618
Davide Colombo <davide.colombo02@protonmail.com>
2023-08-04 16:21:45 +0200

    getline.c: different version, using dynamic memory

--- getline.c
+++ getline.c
@@ -1,43 +1,49 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-#define MAXLINE     1000
+#define MAXLINE     (size_t) 1000
 
-static int mygetline(char line[], int32_t lim);
+static size_t mygetline(char *s, size_t lim);
 
 int main(int argc, char *argv[]){
 
-    int32_t len;
-    char line[MAXLINE];
-    while((len = mygetline(line, MAXLINE)) > 0){
-        printf("%s\n", line);
-        printf("len = %d\n", len);
+    char *lp;
+    lp = malloc(sizeof(char) * MAXLINE);
+    if(lp == NULL){
+        printf("error\n");
+        return 1;
     }
+
+    size_t len;
+    while((len = mygetline(lp, MAXLINE)) > 0){
+        puts(lp);
+        printf("len = %zu\n", len);
+    }
+
+    if(lp != NULL)
+        free((void *)lp);
 
     return 0;
 }
 
 // internal linkage
-static int mygetline(char s[], int32_t lim){
+static size_t mygetline(char *s, size_t lim){
 
-    int32_t c, i;
-
+    int32_t c;
+    size_t i;
     i = 0;
-    lim -= 2;
+    lim -= 1;
     do{
         c = getchar();
-        if(c == EOF){
-            break;
-        }else{
-            s[i] = c;
-            i++;
-        }
-
-        if(i >= lim || c == '\n') break;
-
-    }while(1);
-
+        s[i] = c;
+        i++;
+    }while(c != '\n' && c != EOF && i < lim);
+    
+    if(c == EOF){
+        i -= 1;
+    }
     s[i] = '\0';
     return i;
 }
