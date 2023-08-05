50eb9a0a91f9884c83a95e40b1f1b89cf12a49c2
Davide Colombo <davide.colombo02@protonmail.com>
2023-08-05 18:47:56 +0200

    low_or_up.c: first version

---
+++ low_or_up.c
@@ -0,0 +1,110 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+static char *valid_low[] = {"l", "low", "lower"};
+static char *valid_up[] = {"u", "up", "upper"};
+
+static size_t strpos(const char *src, char c);
+static char *stripch(const char *src, char c);
+static char *stripat(const char *src, size_t pos);
+
+// ============================================================================
+// main
+int main(int argc, char **argv){
+    
+    return 0;
+}
+
+// ============================================================================
+//stripat
+static char *stripat(const char *src, size_t pos){
+    size_t srclen = lenof(src);
+
+    // to avoid realloc later, allocate memory for 1 byte less!
+    char *dst = malloc(srclen);
+    if(dst == NULL){
+        fprintf(stderr, "stripat(): malloc failed\n");
+        return NULL;
+    }
+
+    char *tmp = src;
+    char *dtmp = dst;
+    char *trg = src+pos;
+    int src_ch;
+    do{
+        src_ch = *tmp;
+        if(tmp == trg){
+            ++tmp;
+            continue;
+        }
+
+        *dtmp = src_ch;
+        if(src_ch == '\0') break;
+        ++tmp, ++dtmp;
+    }while(1);
+
+    return dst;
+}
+
+// ============================================================================
+// strpos
+static size_t strpos(const char *src, char c){
+    size_t pos;
+    const char *tmp;
+    for(pos = 0, tmp = src; tmp != '\0' && tmp != c; ++pos, ++tmp)
+        ;
+    return pos;
+}
+
+// ============================================================================
+// stripch
+static char *stripch(const char *src, char c){
+    size_t srclen = lenof(src);
+    char *dst = malloc(srclen+1);
+    if(dst == NULL){
+        fprintf(stderr, "stripch(): malloc failed\n");
+        return NULL;
+    }
+    
+    char *tmp = src;
+    char *dtmp = dst;
+    int src_ch;
+    do{
+        src_ch = *tmp;
+        while(src_ch == c){
+            ++tmp;
+            src_ch = *tmp;
+        }
+        
+        // copy onto destination
+        *dtmp = src_ch;
+        if(src_ch == '\0') break;
+        ++dtmp, ++tmp;
+    }while(1);
+
+    // realloc
+    size_t dstlen = lenof(dst);
+    if(dstlen < srclen){
+        char *tmp = realloc(dst, dstlen+1);
+        if(tmp == NULL){
+            fprintf(stderr, "stripch(): realloc failed\n");
+            return NULL;
+        }
+        dst = tmp;
+    }
+
+    return dst;
+}
+
+// ============================================================================
+static size_t lenof(const char *src){
+    size_t index;
+    // load address before the cycle
+    char *tmp = src;
+    for(index = 0; tmp != '\0'; ++index, ++tmp)
+        ;
+    return index;
+}
