fa5e99d092400fb4f2f7c754cad741c54ba596ec
Davide Colombo <davide.colombo02@protonmail.com>
2023-08-06 15:24:28 +0200

    ptrdiff.c: inspecting 'ptrdiff_t'

---
+++ ptrdiff.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+
+int main(int argc, char **argv){
+
+    const size_t N = 100;
+    int numbers[N];
+
+    printf("PTRDIFF_MAX = %ld\n", PTRDIFF_MAX);
+    int *p1 = &numbers[18];
+    int *p2 = &numbers[23];
+
+    ptrdiff_t diff = p2 - p1;
+    printf("p2 - p1 = %td\n", diff);
+
+    return 0;
+}
