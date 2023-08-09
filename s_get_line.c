619dcdc7e828545f6a3dbbc76c656dcfa6f9be89
Davide Colombo <davide.colombo02@protonmail.com>
2023-08-09 18:31:44 +0200

    s_get_line.c: understood how getchar() works
    getchar() continues to read the characters until it encounters
    an EOF.
    
    This is because getchar() has an internal buffer.
    
    This means that, even if we exhausted the line buffer for the
    CURRENT FUNCTION CALL, and we call that function another time,
    and there are other characters in the getchar() function's
    internal buffer, those characters are immediately used.
    
    So, the behavior is that, getchar() does not prompt you to
    type in other characters until it exhausts its own internal
    buffer.

--- s_get_line.c
+++ s_get_line.c
@@ -1,8 +1,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define S_GET_LINE_BUFSIZE 16
+#define S_GET_LINE_BUFSIZE 64
+
+typedef enum {
+    S_GET_LINE_SUCCESS = 0,
+    S_GET_LINE_NULL_BUFFER,
+    S_GET_LINE_ZERO_BUFSIZE,
+    S_GET_LINE_INVALID_BUFIDX
+} err_t;
+
+char *error_msg[] = {
+    "success",
+    "invalid 'buffer': NULL",
+    "invalid 'bufsize': 0",
+    "invalid 'bufidx': >= 'bufsize'"
+};
 
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  * 'buffer' should point to a writable memory location;
@@ -25,34 +40,53 @@
  * 
  * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
 
-static char *s_get_line_write(char *buffer, size_t bufsize);
-static char *s_get_line_append(char *buffer, size_t bufsize, size_t bufidx);
+static err_t s_get_line_write(char *buffer, size_t bufsize);
+static err_t s_get_line_append(char *buffer, size_t bufsize, size_t bufidx);
 
 // Statically allocated arrays MUST NOT be declared as LOCAL VARIABLES.
 char buffer[S_GET_LINE_BUFSIZE];
+char initialized_buffer[] = "Hello, this is a very happy day!";
 
 // ============================================================================
 int main(int argc, char **argv){
     
+    fprintf(stdout, "\n<TEST WRITE>\n");
+
     // char *buffer;       /* DANGEROUS */
+    err_t status;
 
     // Test 1
-    if( s_get_line_write(buffer, S_GET_LINE_BUFSIZE) == NULL )
-        fprintf(stderr, "failed\n");
+    if( (status = s_get_line_write(buffer, S_GET_LINE_BUFSIZE)) != S_GET_LINE_SUCCESS )
+        fprintf(stderr, "%s\n", error_msg[status]);
+    puts(buffer);
+
+    if( (status = s_get_line_write(buffer, S_GET_LINE_BUFSIZE)) != S_GET_LINE_SUCCESS )
+        fprintf(stderr, "%s\n", error_msg[status]);
     puts(buffer);
 
     // Test NULL
-    if( s_get_line_write(NULL, 10) == NULL )
-        fprintf(stderr, "failed\n");
+    if( (status = s_get_line_write(NULL, 10)) != S_GET_LINE_SUCCESS )
+        fprintf(stderr, "%s\n", error_msg[status]);
 
     // Test 'bufsize' 0
-    if( s_get_line_write(buffer, 0) == NULL )
-        fprintf(stderr, "failed\n");
+    if( (status = s_get_line_write(buffer, 0)) != S_GET_LINE_SUCCESS)
+        fprintf(stderr, "%s\n", error_msg[status]);
 
     // Test bufsize much larger than actual 'buffer' size
     // this is UNDEFINED and DANGEROUS
     //if( s_get_line_write(buffer, 128) == NULL )
     //    fprintf(stderr, "failed\n");
+
+    fprintf(stdout, "\n<TEST APPEND>\n");
+    size_t init_buf_size = sizeof initialized_buffer;
+
+    if( (status = s_get_line_append(initialized_buffer, init_buf_size, 16)) != S_GET_LINE_SUCCESS )
+        fprintf(stderr, "%s\n", error_msg[status]);
+    puts(initialized_buffer);
+
+    if( (status = s_get_line_append(initialized_buffer, init_buf_size, 0)) != S_GET_LINE_SUCCESS )
+        fprintf(stderr, "%s\n", error_msg[status]);
+    puts(initialized_buffer);
 
     return 0;
 }
@@ -61,15 +95,13 @@ int main(int argc, char **argv){
 /* Read at most 'bufsize - 1' bytes, overwrite the content of 'buffer', 
  * do not reallocate.
  */
-static char *s_get_line_write(char *buffer, size_t bufsize){
+static err_t s_get_line_write(char *buffer, size_t bufsize){
     if(buffer == NULL){
-        // set a specific error code
-        return NULL;
+        return S_GET_LINE_NULL_BUFFER;
     }
 
     if(bufsize == 0){
-        // set a specific error code
-        return NULL;
+        return S_GET_LINE_ZERO_BUFSIZE;
     }
 
     do{
@@ -83,21 +115,50 @@ static char *s_get_line_write(char *buffer, size_t bufsize){
         int tmpchar2 = (eol_mask & '\0') | (~eol_mask & tmpchar1);
         int tmpchar3 = (eob_mask & '\0') | (~eob_mask & tmpchar2);
 
+        int tmpmove = (eob_mask & 0) | (~eob_mask & 1);
+
         *buffer = tmpchar3;
-        if(!tmpchar3) break;    // first store, then break
-        buffer += 1;
-        bufsize -= 1;
+
+        if(!tmpchar3) break;
+
+        buffer += tmpmove;
+        bufsize -= tmpmove;
     }while(1);
 
-    return buffer;
+    return S_GET_LINE_SUCCESS;
 }
 
 // ============================================================================
 /* Read at most 'bufsize - bufidx - 1' bytes, append the content starting at 
  * 'bufidx', do not reallocate 
  */
-static char *s_get_line_append(char *buffer, size_t bufsize, size_t bufidx){
-    if(buffer == NULL) return NULL;
+static err_t s_get_line_append(char *buffer, size_t bufsize, size_t bufidx){
+    if(buffer == NULL) return S_GET_LINE_NULL_BUFFER;
+    if(bufsize == 0) return S_GET_LINE_ZERO_BUFSIZE;
+    if(bufidx >= bufsize) return S_GET_LINE_INVALID_BUFIDX;
 
-    return buffer;
+    bufsize -= bufidx;
+    buffer += bufidx;
+    do{
+        int tmpc = getchar();
+
+        uint32_t eof_mask = (uint32_t) (((int32_t)(tmpc == EOF) << 31) >> 31);
+        uint32_t eol_mask = (uint32_t) (((int32_t)(tmpc == '\n') << 31) >> 31);
+        uint32_t eob_mask = (uint32_t) (((int32_t)(bufsize <= 1) << 31) >> 31);
+
+        int tmpchar1 = (eof_mask & '\0') | (~eof_mask & tmpc);
+        int tmpchar2 = (eol_mask & '\0') | (~eol_mask & tmpchar1);
+        int tmpchar3 = (eob_mask & '\0') | (~eob_mask & tmpchar2);
+
+        int tmpmove = (eob_mask & 0) | (~eob_mask & 1);
+
+        *buffer = tmpchar3;
+
+        if(!tmpchar3) break;
+
+        buffer += tmpmove;
+        bufsize -= tmpmove;
+    }while(1);
+
+    return S_GET_LINE_SUCCESS;
 }
